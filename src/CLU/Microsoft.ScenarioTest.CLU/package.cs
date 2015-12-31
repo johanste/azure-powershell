5b50cbd3c493114052b60d7f1fb1fefa81bb891f
Johan Stenberg <johan.stenberg@microsoft.com>
2015-12-31 14:05:31 -0800

    Add missing file

---
+++ src/CLU/Microsoft.ScenarioTest.CLU/package.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Microsoft.CLU.Test
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var baseDirectory = new System.IO.DirectoryInfo(AppContext.BaseDirectory);
+            var config = new Microsoft.CLU.Common.PackageConfig(
+                "Microsoft.ScenarioTests.CLU",
+                new System.Reflection.Assembly[] { typeof(Program).GetTypeInfo().Assembly }
+                );
+
+
+            return Microsoft.CLU.CommandPackage.CommandPackage.Handle(config, baseDirectory.Parent.Parent, args);
+        }
+    }
+}
